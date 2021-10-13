cc0bff468d734b30c7853a304563fe05401be829
Michael Hagan <michael.hagan@amalitech.org>
2021-10-13 23:05:04 +0000

    first commit

---
+++ Whale Talk.js
@@ -0,0 +1,33 @@
+const input = 'Amalitech Ghana';
+
+const vowels = ['a','e','i','o','u'];
+
+let resultArray=[];
+
+for(let inputIndex =0; inputIndex<input.length; inputIndex++){
+
+//console.log(i);
+for(let vowelsIndex=0; vowelsIndex<vowels.length; vowelsIndex++ ){
+
+  if(input[inputIndex] === vowels[vowelsIndex]){
+
+if(input[inputIndex] === 'e'){
+resultArray.push('ee');
+
+}else if(input[inputIndex] === 'u'){
+  resultArray.push('uu');
+  
+  }else{
+
+      resultArray.push(input[inputIndex]);
+
+}
+
+  }
+
+}
+  
+}
+
+console.log(resultArray.join('').toUpperCase() );
+
